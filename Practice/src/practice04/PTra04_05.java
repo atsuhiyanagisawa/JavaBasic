bf8a20390ceec26189b63b34add8f064f4efd0db
atsushi-yanagisawa <atsushikun0815@gmail.com>
2018-02-14 15:43:31 +0900

    practice4

--- Practice/src/practice04/PTra04_05.java
+++ Practice/src/practice04/PTra04_05.java
@@ -46,17 +46,26 @@ public class PTra04_05 {
 			 */
 			if (cpuHand == 0) {
 				System.out.println("グー！");
-
+				if(myHand == 2) {
+					System.out.println("あなたの勝ちです、ゲームを終了します。");
+					break;
+				}
 
 
 			} else if (cpuHand == 1) {
 				System.out.println("チョキ！！");
-
+				if(myHand == 0) {
+					System.out.println("あなたの勝ちです、ゲームを終了します。");
+					break;
+				}
 
 
 			} else {
 				System.out.println("パー！！");
-
+				if(myHand == 1) {
+					System.out.println("あなたの勝ちです、ゲームを終了します。");
+					break;
+				}
 
 
 			}
